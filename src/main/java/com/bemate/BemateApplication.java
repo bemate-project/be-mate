1ee84a54b05b0b540c96358f95d84fe272ccf580
solmioh <ohsolmioh@gmail.com>
2023-06-05 15:02:29 +0900

    first commit

---
+++ src/main/java/com/bemate/BemateApplication.java
@@ -0,0 +1,13 @@
+package com.bemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bemate/BemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
